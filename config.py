6aed653b7e290543b5c27c1aa96b2aa4fd488279
Steve <solinvic@gmail.com>
2024-08-21 20:26:46 -0600

    Update element wattage

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ listening_port = 8081
 # to produce the actual cost during a run. My kiln has three
 # elements that when my switches are set to high, consume 9460 watts.
 kwh_rate        = 0.1319  # cost per kilowatt hour per currency_type to calculate cost to run job
-kw_elements     = 4.320 # if the kiln elements are on, the wattage in kilowatts
+kw_elements     = 5.520 # if the kiln elements are on, the wattage in kilowatts
 currency_type   = "$"   # Currency Symbol to show when calculating cost to run job
 
 ########################################################################
